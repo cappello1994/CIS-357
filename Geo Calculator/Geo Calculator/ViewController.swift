5f86746c152807afb5d64c49e775c7e589ca1616
Sierracy <ellisosi@mail.gvsu.edu>
2017-09-20 12:14:16 -0400

    Started Geo Calculator

---
+++ Geo Calculator/Geo Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Geo Calculator
+//
+//  Created by X Code User on 9/20/17.
+//  Copyright © 2017 GVSU School of Computing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

4b59e5939fd48e2035313a1cc2e9cee83b41b279
X Code User <xcodeuser@admins-iMac-285.local>
2017-09-21 15:58:17 -0400

    Please enter the commit message for your changes. Lines starting Added clear button functionality and empty string detection

--- Geo Calculator/Geo Calculator/ViewController.swift
+++ Geo Calculator/Geo Calculator/ViewController.swift
@@ -10,10 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var latitudeField1: UITextField!
+    
+    @IBOutlet weak var latitudeField2: UITextField!
+    
+    @IBOutlet weak var longitudeField1: UITextField!
+    
+    @IBOutlet weak var longitudeField2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        //TODO ask if you need to use self??
+        self.latitudeField1.text = ""
+        self.latitudeField2.text = ""
+        self.longitudeField1.text = ""
+        self.longitudeField2.text = ""
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        print("Calculating")
+        if let lat1 = self.latitudeField1.text{
+            if lat1 != ""{
+                print("Text received: \(lat1)")
+            }
+            
+            else{
+                print("Field cannot be blank")
+            }
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

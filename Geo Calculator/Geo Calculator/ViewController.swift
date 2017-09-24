ce50513a30eff2ddfda78969bb49f03ef3d519a5
X Code User <xcodeuser@admins-iMac-339.local>
2017-09-24 13:21:40 -0400

    Added math in calculation functions Distance in km, bearing in degrees

--- Geo Calculator/Geo Calculator/ViewController.swift
+++ Geo Calculator/Geo Calculator/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Foundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var latitudeField1: UITextField!
@@ -22,7 +22,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+        
     }
     
     @IBAction func clearButtonPressed(_ sender: UIButton) {
@@ -37,18 +39,65 @@ class ViewController: UIViewController {
     
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
         print("Calculating")
-        if let lat1 = self.latitudeField1.text{
-            if lat1 != ""{
-                self.distanceResultField.text = "Result"
-                self.bearingResultField.text = "Result"
-            }
-            
-            else{
-                print("Field cannot be blank")
+        calculateDistance()
+        calculateBearing()
+    }
+    
+    func calculateDistance(){
+        if let lat1 = self.latitudeField1.text, let lat2 = self.latitudeField2.text, let lon1 = self.longitudeField1.text, let lon2 = self.longitudeField2.text{
+            if( lat1 != "" && lat2 != "" && lon1 != "" && lon2 != "" ){
+                let R = 6371.0
+                let dlat = (Double(lat2)! - Double(lat1)!) * (Double.pi/180)
+                let dlon = Double(lon2)! - Double(lon1)! * (Double.pi/180)
+                
+                
+                let a = sin(dlat/2) * sin(dlat/2)
+                
+                let b = cos(Double(lat1)!*(Double.pi/180)) * cos(Double(lat2)!*(Double.pi/180)) * sin(dlon/2) * sin(dlon/2)
+                
+                let c = a + b
+                
+                let d = 2 * atan2(sqrt(c), sqrt(1-c))
+                
+                let e = R * d //Distance in km
+                
+                let distance = round( e * 100 ) / 100
+                
+                self.distanceResultField.text = "\(distance) km"
+                
+                
             }
         }
     }
     
+    func calculateBearing(){
+        if let lat1 = self.latitudeField1.text, let lat2 = self.latitudeField2.text, let lon1 = self.longitudeField1.text, let lon2 = self.longitudeField2.text{
+            if( lat1 != "" && lat2 != "" && lon1 != "" && lon2 != "" ){
+                
+                
+                
+                let lonTheta1 = Double(lon1)! * (Double.pi/180)
+                let lonTheta2 = Double(lon2)! * (Double.pi/180)
+                
+                
+                let dlon = lonTheta2 - lonTheta1
+                
+                let x = sin(dlon) * cos(Double(lat2)!)
+                let y = cos(Double(lat1)!) * sin(Double(lat2)!) - sin(Double(lat1)!) * cos(Double(lat2)!) * cos(dlon)
+                
+                let z = atan2(y,x) * (180/Double.pi)
+                
+                let bearing = round( 100 * z ) / 100 
+                self.bearingResultField.text = "\(bearing) deg"
+                
+                
+            }
+        }
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
